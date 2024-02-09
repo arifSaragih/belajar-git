f3f06f2e8cd57803186d1ef339ff20e596941315
arifsrg <arifabdillah898@gmail.com>
2024-02-09 10:04:18 +0700

    Merubah file test.java

--- test.java
+++ test.java
@@ -1,5 +1,6 @@
 public class test {
   public static void main(string[]args){
-    system.out.println("Hellow Wordl");
+  system.out.println("Hellow Wordl");
+  system.out.printlns("mencoba push ke github");
   }
 }
